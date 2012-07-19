f32c90b8b44a9800f0e5e57b6bc546638bf6ea90
Hyungsuk Hong <aanoaa@gmail.com>
2012-07-19 16:11:31 +0900

    reset indent to '2 space' in list.js

--- root/static/scripts/list.js
+++ root/static/scripts/list.js
@@ -1,12 +1,12 @@
 $('#head_checkbox').click(function() {
-	//find all checkboxes
-	var cbs = $("input:checkbox"); 
-	//화면상의 모든 Checkbox 를 찾아서 Head Checkbox 상태와 동기화 합니다. (대장이 채크면 쫄도 모두다 채크)
-	 for (var i = 0; i < cbs.length; i++) {  
-	    if (cbs[i].type == "checkbox") {  
-	        cbs[i].checked = $(this).is(':checked');
-	    }  
-	 }  
+  //find all checkboxes
+  var cbs = $("input:checkbox"); 
+  //화면상의 모든 Checkbox 를 찾아서 Head Checkbox 상태와 동기화 합니다. (대장이 채크면 쫄도 모두다 채크)
+  for (var i = 0; i < cbs.length; i++) {  
+	if (cbs[i].type == "checkbox") {  
+	  cbs[i].checked = $(this).is(':checked');
+	}  
+  }  
 });
 
 $('#do_delete').click(function() {
