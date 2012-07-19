b5917150f7bfef1adeb536f9ef09fb6540b2fe15
Hyungsuk Hong <aanoaa@gmail.com>
2012-07-19 16:12:57 +0900

    wrap code `$(document).ready(function () {})` in list.js

--- root/static/scripts/list.js
+++ root/static/scripts/list.js
@@ -1,54 +1,56 @@
-$('#head_checkbox').click(function() {
-  //find all checkboxes
-  var cbs = $("input:checkbox"); 
-  //화면상의 모든 Checkbox 를 찾아서 Head Checkbox 상태와 동기화 합니다. (대장이 채크면 쫄도 모두다 채크)
-  for (var i = 0; i < cbs.length; i++) {  
-	if (cbs[i].type == "checkbox") {  
-	  cbs[i].checked = $(this).is(':checked');
-	}  
-  }  
-});
-
-$('#do_delete').click(function() {
-  var selected_charges = [];
-
-  $('#charge_list tr').filter(':has(:checkbox:checked)').each(function(){
-    if($(this).attr('id') !== undefined)
-      selected_charges.push($(this).attr('id'))
+$(document).ready(function () {
+  $('#head_checkbox').click(function() {
+    //find all checkboxes
+    var cbs = $("input:checkbox"); 
+    //화면상의 모든 Checkbox 를 찾아서 Head Checkbox 상태와 동기화 합니다. (대장이 채크면 쫄도 모두다 채크)
+    for (var i = 0; i < cbs.length; i++) {  
+	  if (cbs[i].type == "checkbox") {  
+	    cbs[i].checked = $(this).is(':checked');
+	  }  
+    }  
   });
 
-  location.href = '/list/delete/' + selected_charges;
-});
+  $('#do_delete').click(function() {
+    var selected_charges = [];
 
-$('#do_approval').click(function() {
-  var selected_charges = [];
+    $('#charge_list tr').filter(':has(:checkbox:checked)').each(function(){
+      if($(this).attr('id') !== undefined)
+        selected_charges.push($(this).attr('id'))
+    });
 
-  $('#charge_list tr').filter(':has(:checkbox:checked)').each(function(){
-    if($(this).attr('id') !== undefined)
-      selected_charges.push($(this).attr('id'))
+    location.href = '/list/delete/' + selected_charges;
   });
 
-  location.href = '/list/approval/' + selected_charges;
-});
+  $('#do_approval').click(function() {
+    var selected_charges = [];
 
-$('#do_refuse').click(function() {
-  var selected_charges = [];
+    $('#charge_list tr').filter(':has(:checkbox:checked)').each(function(){
+      if($(this).attr('id') !== undefined)
+        selected_charges.push($(this).attr('id'))
+    });
 
-  $('#charge_list tr').filter(':has(:checkbox:checked)').each(function(){
-    if($(this).attr('id') !== undefined)
-      selected_charges.push($(this).attr('id'))
+    location.href = '/list/approval/' + selected_charges;
   });
 
-  location.href = '/list/refuse/' + selected_charges;
-});
+  $('#do_refuse').click(function() {
+    var selected_charges = [];
 
-$('#do_deposit').click(function() {
-  var selected_charges = [];
+    $('#charge_list tr').filter(':has(:checkbox:checked)').each(function(){
+      if($(this).attr('id') !== undefined)
+        selected_charges.push($(this).attr('id'))
+    });
 
-  $('#charge_list tr').filter(':has(:checkbox:checked)').each(function(){
-    if($(this).attr('id') !== undefined)
-      selected_charges.push($(this).attr('id'))
+    location.href = '/list/refuse/' + selected_charges;
   });
 
-  location.href = '/deposit/approval/' + selected_charges;
+  $('#do_deposit').click(function() {
+    var selected_charges = [];
+
+    $('#charge_list tr').filter(':has(:checkbox:checked)').each(function(){
+      if($(this).attr('id') !== undefined)
+        selected_charges.push($(this).attr('id'))
+    });
+
+    location.href = '/deposit/approval/' + selected_charges;
+  });
 });
