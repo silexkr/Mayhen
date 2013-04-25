82d9fe185808113feef0ca392e34b6330f17ac79
Hanyoung Cho <rumidier@gmail.com>
2013-04-25 19:00:35 +0900

    History 테이블에 컬럼 추가후 dbicdump 실행

--- lib/Silex/Donnenwa/Schema/Result/History.pm
+++ lib/Silex/Donnenwa/Schema/Result/History.pm
@@ -109,6 +109,12 @@ __PACKAGE__->table("history");
   data_type: 'text'
   is_nullable: 0
 
+=head2 history_status
+
+  data_type: 'integer'
+  default_value: 0
+  is_nullable: 0
+
 =cut
 
 __PACKAGE__->add_columns(
@@ -182,6 +188,8 @@ __PACKAGE__->add_columns(
   },
   "memo",
   { data_type => "text", is_nullable => 0 },
+  "history_status",
+  { data_type => "integer", default_value => 0, is_nullable => 0 },
 );
 
 =head1 PRIMARY KEY
@@ -214,8 +222,8 @@ __PACKAGE__->belongs_to(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07025 @ 2013-04-22 17:19:46
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:kBehd/hGL5O7dJLBMAvGjw
+# Created by DBIx::Class::Schema::Loader v0.07025 @ 2013-04-25 11:52:24
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:QiCrJjwRnTLM/jnKmv9jnA
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
